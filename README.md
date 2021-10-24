1e96d294706a296a32e9ea55ef69fd4117cb1125
Jazzmin S. Goodman <js.goodman@Jazzmins-MBP.attlocal.net>
2021-10-24 02:58:56 -0700

    add variables and generate function

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # password-generator
+
+## Purpose 
+
+The purpose of this project is to add functionality to a button with javascript, and enabling the user to generate a random password based on their chosen criteria. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,60 @@
-var writePassword = function() {
-   var promptSelectLength = window.prompt("How many characters would you like your password to contain?")
-   if (promptSelectLength < 8) {
-       window.alert("Your password must contain at least 8 characters.")
-   } else if (promptSelectLength > 128) {
-       window.alert("Your password cannot contain more than 128 characters.")
-   } else (promptSelectLength != Numeric(num)) ;{
-       window.alert("You must enter a number.")
-   }
-  
-}
+// Assignment Code 
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacters = "!@#$%^&*()";
 
-// Assignment Code end
+var passwordCharSet = ""
 
 var generateBtn = document.querySelector("#generate");
+    
+var generatePassword = function() {
+    
+    console.log("hey! you clicked the button!");
+// 1. Prompt the user for the password criteria.
+    //     a. password length between 8 and 128
+    //     b. lowercase, uppercase, numbers, special characters 
+    
+        var promptSelectLength = window.prompt("How many characters would you like your password to contain?")
+        if (promptSelectLength < 8) {
+            window.alert("Your password must contain at least 8 characters.")
+        } else if (promptSelectLength > 128) {
+            window.alert("Your password cannot contain more than 128 characters.")
+        } else if (promptSelectLength >= 8 && promptSelectLength <= 128) {
+            var isLower = window.confirm("Would you like your password to contain lowercase letters?");
+            if (isLower) {
+                passwordCharSet = passwordCharSet + lowercase
+        } 
+            var isUpper = window.confirm("Would you like your password to contain uppercase letters?");
+            if (isUpper) {
+                passwordCharSet = passwordCharSet + uppercase
+            }
+            var isNumber = window.confirm("Would you like your password to contain numbers?");
+            if (isNumber) {
+                passwordCharSet = passwordCharSet + numbers
+        } 
+            var isSpecial = window.confirm("Would you like your password to contain special characters?");
+            if (isSpecial) {
+                passwordCharSet = passwordCharSet + specialCharacters
+            } 
+            var result = ""
+        for (var i = 0; i < promptSelectLength; i++) {
+            result += passwordCharSet.charAt(Math.floor(Math.random() * passwordCharSet.length));
 
+        }
+        return result;
+    }
+        else {
+            window.alert("You must enter a number.")
+        }
+        
+        // 2 Validate input 
+        // 3 Generate password based on criteria
+        // 4 Display generated password to the page
+        
+}
+            
+        
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -27,3 +68,18 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // list out steps in order to define function
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+        
